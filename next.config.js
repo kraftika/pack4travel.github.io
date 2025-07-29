4722f5744104e7f80cc339f1f3e2e047d2a344f8
Ionut Pistol <ionut.pistol.dev@gmail.com>
2025-07-29 14:49:17 +0300

    Fix deployment

--- next.config.js
+++ next.config.js
@@ -1,10 +1,10 @@
 const isProd = process.env.NODE_ENV === "production";
-const repoName = "pack4travel";
+const repoName = "pack4travel.github.io";
 
 const nextConfig = {
   output: "export",
-  basePath: isProd ? "/pack4travel" : "",
-  assetPrefix: isProd ? "/pack4travel/" : "",
+  basePath: isProd ? `/${repoName}` : "",
+  assetPrefix: isProd ? `/${repoName}` : "",
 };
 
 module.exports = nextConfig;
